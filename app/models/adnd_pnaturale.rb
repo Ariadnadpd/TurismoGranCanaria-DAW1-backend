8b08e74fc87fba9b1140267e858a7ff7f36dea36
Ariadnadpd <ari.ddpd@gmail.com>
2021-12-10 22:35:03 +0100

    Añadido JSON (Adondeir) y algunas modificaciones

--- app/models/adnd_pnaturale.rb
+++ app/models/adnd_pnaturale.rb
@@ -2,7 +2,7 @@ class AdndPnaturale < ApplicationRecord
     validates :titulo,
         presence: {presence: true, message: ': ¡El título no puede estar en blanco, es un campo requerido!'}, 
         length: { maximum: 50, message: ': ¡El título debe de tener como máximo 50 caracteres!'},
-        length: { minimum: 5, message: ': ¡El título debe de tener como mínimo 5 caracteres!'}
+        length: { minimum: 4, message: ': ¡El título debe de tener como mínimo 5 caracteres!'}
     validates :subtitulo,
         presence: {presence: true, message: ': ¡El subtítulo no puede estar en blanco, es un campo requerido!'},
         length: { maximum: 50, message: ': ¡El subtítulo debe de tener como máximo 50 caracteres!'},
